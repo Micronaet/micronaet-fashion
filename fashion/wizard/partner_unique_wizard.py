adab7f24af5b20fb864a3b50f9268d9d285350a1
nicothebrush <nicola.riolini@gmail.com>
2017-03-07 19:27:21 +0100

    remove comment

--- fashion/wizard/partner_unique_wizard.py
+++ fashion/wizard/partner_unique_wizard.py
@@ -117,47 +117,6 @@ class ResPartnerUniqueNameWizard(orm.TransientModel):
             ('sale_order', 'partner_id'),
             ('sale_order', 'partner_invoice_id'),
             ('sale_order', 'partner_shipping_id'),
-            # "account_analytic_account" CONSTRAINT "account_analytic_account_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "account_bank_statement_line" CONSTRAINT "account_bank_statement_line_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "account_invoice" CONSTRAINT "account_invoice_commercial_partner_id_fkey" FOREIGN KEY (commercial_partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "account_invoice_line" CONSTRAINT "account_invoice_line_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "account_invoice" CONSTRAINT "account_invoice_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "account_model_line" CONSTRAINT "account_model_line_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "account_move_line" CONSTRAINT "account_move_line_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE RESTRICT
-            #TABLE "account_move" CONSTRAINT "account_move_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "account_partner_reconcile_process" CONSTRAINT "account_partner_reconcile_process_next_partner_id_fkey" FOREIGN KEY (next_partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "account_voucher" CONSTRAINT "account_voucher_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "fashion_form_accessory_pricelist" CONSTRAINT "fashion_form_accessory_pricelist_supplier_id_fkey" FOREIGN KEY (supplier_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "fashion_form_accessory_rel" CONSTRAINT "fashion_form_accessory_rel_supplier_id_fkey" FOREIGN KEY (supplier_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "fashion_form_cost_rel_pricelist" CONSTRAINT "fashion_form_cost_rel_pricelist_suplier_id_fkey" FOREIGN KEY (suplier_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "fashion_form_cost_rel_pricelist" CONSTRAINT "fashion_form_cost_rel_pricelist_supplier_id_fkey" FOREIGN KEY (supplier_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "fashion_form_fabric" CONSTRAINT "fashion_form_fabric_supplier_id_fkey" FOREIGN KEY (supplier_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "fashion_form" CONSTRAINT "fashion_form_group_id_fkey" FOREIGN KEY (group_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "fashion_form_partner_rel" CONSTRAINT "fashion_form_partner_rel_group_id_fkey" FOREIGN KEY (group_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "fashion_form_partner_rel" CONSTRAINT "fashion_form_partner_rel_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "fashion_form_partner_rel" CONSTRAINT "fashion_form_partner_rel_supplier_id_fkey" FOREIGN KEY (supplier_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "mail_compose_message" CONSTRAINT "mail_compose_message_author_id_fkey" FOREIGN KEY (author_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "mail_compose_message_res_partner_rel" CONSTRAINT "mail_compose_message_res_partner_rel_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "mail_followers" CONSTRAINT "mail_followers_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "mail_message" CONSTRAINT "mail_message_author_id_fkey" FOREIGN KEY (author_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "mail_message_res_partner_rel" CONSTRAINT "mail_message_res_partner_rel_res_partner_id_fkey" FOREIGN KEY (res_partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "mail_notification" CONSTRAINT "mail_notification_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "mail_wizard_invite_res_partner_rel" CONSTRAINT "mail_wizard_invite_res_partner_rel_res_partner_id_fkey" FOREIGN KEY (res_partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "portal_wizard_user" CONSTRAINT "portal_wizard_user_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "product_supplierinfo" CONSTRAINT "product_supplierinfo_name_fkey" FOREIGN KEY (name) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "res_company" CONSTRAINT "res_company_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "res_partner_bank" CONSTRAINT "res_partner_bank_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "res_partner" CONSTRAINT "res_partner_group_id_fkey" FOREIGN KEY (group_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "res_partner" CONSTRAINT "res_partner_parent_id_fkey" FOREIGN KEY (parent_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "res_partner_res_partner_category_rel" CONSTRAINT "res_partner_res_partner_category_rel_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "res_partner_unique_name_wizard" CONSTRAINT "res_partner_unique_name_wizard_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE CASCADE
-            #TABLE "res_request" CONSTRAINT "res_request_ref_partner_id_fkey" FOREIGN KEY (ref_partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "res_users" CONSTRAINT "res_users_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE RESTRICT
-            #TABLE "sale_order_line" CONSTRAINT "sale_order_line_address_allotment_id_fkey" FOREIGN KEY (address_allotment_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "sale_order_line" CONSTRAINT "sale_order_line_order_partner_id_fkey" FOREIGN KEY (order_partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "sale_order" CONSTRAINT "sale_order_partner_id_fkey" FOREIGN KEY (partner_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "sale_order" CONSTRAINT "sale_order_partner_invoice_id_fkey" FOREIGN KEY (partner_invoice_id) REFERENCES res_partner(id) ON DELETE SET NULL
-            #TABLE "sale_order" CONSTRAINT "sale_order_partner_shipping_id_fkey" FOREIGN KEY (partner_shipping_id) REFERENCES res_partner(id) ON DELETE SET NULL            
             )
         deactivate_ids = []        
         for name in unify_name:
@@ -173,7 +132,6 @@ class ResPartnerUniqueNameWizard(orm.TransientModel):
                     cr.execute(query)                
                 except:
                     _logger.error('%s %s' % (table, field))
-                    import pdb; pdb.set_trace()
             deactivate_ids.extend(remove_ids)
         
         import pdb; pdb.set_trace()
