e333526638e7864536a6bfcaba430c32751d95ff
nicothebrush <nicola.riolini@gmail.com>
2023-12-07 16:41:36 +0100

    generic bugfix (fast message)

--- procedure/LancioTot/genera_lancio_excel.py
+++ procedure/LancioTot/genera_lancio_excel.py
@@ -548,9 +548,9 @@ for master_key in file_data['master']:
 
 excel_line = file_data['total_tg'][:]
 excel_line.append(file_data['total'])
-Excel.write_xls_line(
-    detail_page, row, [(v, f_text_title_center) for v in excel_line], f_text,
-    col=fixed_side['left'] + fixed_side['center'])
+#Excel.write_xls_line(
+#    detail_page, row, [(v, f_text_title_center) for v in excel_line], f_text,
+#    col=fixed_side['left'] + fixed_side['center'])
 
 # Row height data:
 Excel.row_height(
